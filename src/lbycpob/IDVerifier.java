48ab85eec6b13270ed0eb806638a84eac8bbe424
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:10:25 +0800

    added idverifier class, and verifyid void

--- src/lbycpob/IDVerifier.java
+++ src/lbycpob/IDVerifier.java
@@ -1,1 +1,21 @@
 package lbycpob
+
+public class IDVerifier {
+    public void verifyID() {
+        String continueInput;
+
+        do {
+            IO.print("Enter ID: ");
+            String idNumber = IO.readln().trim();
+
+            IO.println(validateID(idNumber));
+
+            IO.print("Check another ID? (yes/y to continue): ");
+            continueInput = IO.readln().trim().toLowerCase();
+
+        } while (continueInput.equalsIgnoreCase("yes")
+                || continueInput.equalsIgnoreCase("y"));
+
+        IO.println("Exiting ID verification.");
+    }
+}
