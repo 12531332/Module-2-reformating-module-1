fcafce85a448a318964ed9e6c0388c60914b4608
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 09:59:45 +0800

    added all the grade constants

--- src/lbycpob/GradeConstants.java
+++ src/lbycpob/GradeConstants.java
@@ -1,3 +1,22 @@
 package lbycpob
 
 public class GradeConstants {
+
+    public static final int    MAX_STUDENTS       = 20;
+    public static final double MIN_SCORE          = 0.0;
+    public static final double MAX_SCORE          = 100.0;
+    public static final int    SEPARATOR          = 50;
+    public static final int    NUM_MODULES        = 5;
+    public static final int    ID_LENGTH          = 8;
+    public static final int    ID_DIVISOR         = 11;
+    public static final int    FACULTY_THRESHOLD  = 16;
+
+    // [WEIGHT] Component weights for raw grade computation
+    // [TRACE] All five weights must sum to 1.0 (100%)
+    //         0.40 + 0.05 + 0.05 + 0.20 + 0.30 = 1.00
+    public static final double LAB_WEIGHT           = 0.40;
+    public static final double PARTICIPATION_WEIGHT = 0.05;
+    public static final double TEACHER_WEIGHT       = 0.05;
+    public static final double EXAM_WEIGHT          = 0.20;
+    public static final double PROJECT_WEIGHT       = 0.30;
+}
