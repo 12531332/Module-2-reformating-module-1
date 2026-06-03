8b613079b73b03b51beda1edd421c0ea2f400320
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 09:54:57 +0800

    added compute average class

--- src/lbycpob/GradeCalculator.java
+++ src/lbycpob/GradeCalculator.java
@@ -1,2 +1,13 @@
 package lbycpob
 
+public static double computeAverage(double[] scores) {
+
+    if (scores == null || scores.length == 0)
+        return 0.0;
+
+    double sum = 0.0;
+    for (double score : scores) {
+        sum += score;
+    }
+    return sum / scores.length;
+}
