ac05aa51ab0c3c63a083957cedc20c8b0f8120c9
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:19:12 +0800

    added the append string format

--- src/lbycpob/ReportPrinter.java
+++ src/lbycpob/ReportPrinter.java
@@ -20,3 +20,9 @@ public class ReportPrinter {
         sb.append("\n");
         sb.repeat("=", TABLE_WIDTH).append("\n");
 
+        sb.append(String.format(
+                "%-5s %-10s %-20s %-10s %-7s %-6s %-5s%n",
+                "No.", "ID", "Name", "Raw Ave.", "Grade", "Rank", "Remarks"));
+
+        sb.repeat("-", TABLE_WIDTH).append("\n");
+
