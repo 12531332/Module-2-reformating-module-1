91e55c568d26ddf49c56604093897c8ab019bb9f
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:01:51 +0800

    added the display menu and other variables

--- src/lbycpob/GradeTrackerApp.java
+++ src/lbycpob/GradeTrackerApp.java
@@ -1,3 +1,15 @@
 package lbycpob
 
 public class GradeTrackerApp {
+
+    private StudentRepository repo = new StudentRepository();
+    private StudentInputHandler inputHandler = new StudentInputHandler();
+    private IDVerifier idVerifier = new IDVerifier();
+
+    public void main() {
+        // [TRACE] Create one app object, then start the menu loop
+        displayMenu();
+    }
+
+
+}
