2768284a72a3622a08ec7d45c3f4c6a651867ed8
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:32:41 +0800

    added addstudent

--- src/lbycpob/StudentRepository.java
+++ src/lbycpob/StudentRepository.java
@@ -4,3 +4,17 @@ public class StudentRepository {
 
     private Student[] students = new Student[GradeConstants.MAX_STUDENTS];
     private int count = 0;
+
+    public void addStudent(Student s) {
+        // [TRACE] count >= MAX_STUDENTS means the array is full
+        if (count >= GradeConstants.MAX_STUDENTS) {
+            IO.println("Repository is full. Cannot add more students.");
+            return;
+        }
+
+
+        // [TRACE] students[count] = s, then count incremented by 1
+        students[count++] = s;
+
+    }
+
