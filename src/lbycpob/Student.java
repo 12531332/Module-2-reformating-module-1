76545c4eb984070344266b9f53c5a68165c94364
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:23:48 +0800

    added student class

--- src/lbycpob/Student.java
+++ src/lbycpob/Student.java
@@ -1,1 +1,14 @@
-package lbycpob
+package lbycpob;
+
+public class Student {
+    private String name;
+    private String idNumber;
+    private double rawGrade;
+    private String numericGrade;
+    private char letterRank;
+
+    public Student() {
+    }
+
+
+}
