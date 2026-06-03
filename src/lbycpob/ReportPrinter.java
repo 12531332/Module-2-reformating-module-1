1f69a6d90200d558a9db2118012aba105abef065
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:18:36 +0800

    added the start of print report

--- src/lbycpob/ReportPrinter.java
+++ src/lbycpob/ReportPrinter.java
@@ -6,3 +6,17 @@ public class ReportPrinter {
         IO.println("=".repeat(GradeConstants.SEPARATOR));
     }
 
+    public static void printReport(StudentRepository repo) {
+        // [VALIDATE] Guard: no data to display
+        if (repo.getCount() == 0) {
+            IO.println("No data to display. Please enter student data first.");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        final int TABLE_WIDTH = 80;
+
+        sb.append("\n");
+        sb.repeat("=", TABLE_WIDTH).append("\n");
+
