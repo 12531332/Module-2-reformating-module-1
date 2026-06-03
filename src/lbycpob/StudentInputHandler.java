73f71fae9d5606ac3473bc25c7ee1886d7268a3f
12531332 <robert_vinzon_sy@dlsu.edu.ph>
2026-06-03 10:27:35 +0800

    added student input handler class

--- src/lbycpob/StudentInputHandler.java
+++ src/lbycpob/StudentInputHandler.java
@@ -1,1 +1,13 @@
-package lbycpob
+package lbycpob;
+
+public class StudentInputHandler {
+
+    public String inputName(int studentNumber) {
+        String name;
+        do {
+            IO.print("Student " + studentNumber + " name: ");
+            name = IO.readln().trim();
+            if (name.isEmpty()) IO.println("Name cannot be blank. Try again.");
+        } while (name.isEmpty());
+        return name;
+    }
